a60b3fd0bdcfcd3bc21dbb8a8ea82a0026408f61
Ayush Verma <ayushv2909@gmail.com>
2025-04-22 12:56:22 +0530

    Initial commit

---
+++ src/components/About/AboutCard.js
@@ -0,0 +1,43 @@
+import React from "react";
+import Card from "react-bootstrap/Card";
+import { ImPointRight } from "react-icons/im";
+
+function AboutCard() {
+  return (
+    <Card className="quote-card-view">
+      <Card.Body>
+        <blockquote className="blockquote mb-0">
+          <p style={{ textAlign: "justify" }}>
+            Hi Everyone, I am <span className="purple">Ayush Verma </span>
+            from <span className="purple"> Varanasi, UttarPradesh, India.</span>
+            <br />
+            I am currently exploring opportunities in frontend/backend/full stack development.
+            <br />
+            I have completed BSc in Maths and Computer Science at MGKVP University.
+            <br />
+            <br />
+            Apart from coding, some other activities that I love to do!
+          </p>
+          <ul>
+            <li className="about-activity">
+              <ImPointRight /> Video Editing
+            </li>
+            <li className="about-activity">
+              <ImPointRight /> YouTube Blogs
+            </li>
+            <li className="about-activity">
+              <ImPointRight /> Watching Anime
+            </li>
+          </ul>
+
+          <p style={{ color: "rgb(155 126 172)" }}>
+            "Strive to build things that make a difference!"{" "}
+          </p>
+          <footer className="blockquote-footer">Ayush</footer>
+        </blockquote>
+      </Card.Body>
+    </Card>
+  );
+}
+
+export default AboutCard;
